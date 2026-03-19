35c2bf5eba0fc2a443c204870df38b2eadd87167
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-03-18 19:54:58 -0600

    chore: project update

--- src/PCH.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#include "RE/Starfield.h"
-#include "SFSE/SFSE.h"
-
-using namespace std::literals;
--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ namespace
 	}
 }
 
-SFSEPluginLoad(const SFSE::LoadInterface* a_sfse)
+SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse)
 {
 	SFSE::Init(a_sfse);
 
